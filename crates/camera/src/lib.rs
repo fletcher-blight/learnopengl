38b7b6bf72a235cef7854b88b91db2ea3af88789
Fletcher Blight <fletcher.blight@gmail.com>
2023-01-20 11:31:33 +1000

    Fix: Camera initial whiplash

--- crates/camera/src/lib.rs
+++ crates/camera/src/lib.rs
@@ -34,7 +34,7 @@ impl Camera {
             right: glm::vec3(1.0, 0.0, 0.0),
             velocity: glm::vec3(0.0, 0.0, 0.0),
             fov: 45.0,
-            yaw: 0.0,
+            yaw: -90.0,
             pitch: 0.0,
         }
     }
